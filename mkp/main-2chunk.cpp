9e65b10a6c2ef5d773840f8519fdd170dfd196c3
ssssss <ff@f.com>
2025-02-28 15:39:23 -0800

    mejora generador espacio de soluciones

---
+++ mkp/main-2chunk.cpp
@@ -0,0 +1,119 @@
+#include "utils.hpp"
+#include "mkp.hpp"
+#include <Eigen/Dense>
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <cmath>
+#include <omp.h>
+#include <getopt.h>
+#include <vector>
+
+void generarCSV(const std::string& inputFilePath, const std::string& outputFilePath, int numHilos) {
+    int numItems, numDimensiones, optimoConocido;
+    Eigen::VectorXi beneficios;
+    Eigen::MatrixXi pesos;
+    Eigen::VectorXi capacidades;
+    
+    // Leer datos desde archivo
+    try {
+        Utils::leerDatosDesdeArchivo(inputFilePath, numItems, numDimensiones, optimoConocido, beneficios, pesos, capacidades);
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return;
+    }
+    
+    std::cout << "El optimo conocido es: " << optimoConocido << "\n";
+    std::stringstream encabezado;
+    encabezado << "n,f,obj,rel," << Utils::vectorXiToStringVariables(numItems);
+    Utils::escribirLineaCSV(outputFilePath, encabezado.str());
+
+    // Crear instancia del problema
+    Mkp mkp(numItems, numDimensiones, beneficios, pesos, capacidades);
+    
+    // Imprimir el problema
+    mkp.print();
+    int hasta = pow(2, numItems);
+    
+    omp_set_num_threads(numHilos);
+    
+    // Cada hilo almacena su información en memoria antes de escribir
+    std::vector<std::vector<std::string>> buffers(numHilos);
+
+    #pragma omp parallel
+    {
+        int id = omp_get_thread_num();
+        std::vector<std::string> localBuffer;
+
+        #pragma omp for schedule(dynamic)
+        for (int i = 1; i <= hasta; i++) {
+            std::stringstream ss;
+            ss << i << ",";
+            
+            // Obtener la solución binaria
+            Eigen::VectorXi sol = Utils::enteroABinario(i, numItems);
+            // Invertir la solución
+            Eigen::VectorXi solucion = sol.reverse().eval();
+
+            if (mkp.esFactible(solucion)) {
+                ss << "1,";
+            } else {
+                ss << "0,";
+            }
+            
+            int objetivo = mkp.calcularObjetivo(solucion);
+            ss << objetivo << ",";
+            float relativo = static_cast<float>(objetivo) / optimoConocido;
+            ss << std::fixed << std::setprecision(3) << relativo << ","; // Fijar la precisión a 3 decimales
+            ss << Utils::vectorXiToString(solucion);
+            
+            // Guardar en el buffer local del hilo
+            localBuffer.push_back(ss.str());
+        }
+
+        // Guardar los datos del buffer local en el buffer global
+        buffers[id] = std::move(localBuffer);
+    } // Fin de la región paralela
+
+    // Sincronización: escribir en el archivo de forma ordenada
+    for (const auto& buffer : buffers) {
+        for (const auto& linea : buffer) {
+            Utils::escribirLineaCSV(outputFilePath, linea);
+        }
+    }
+}
+
+int main(int argc, char* argv[]) {
+    std::string inputFilePath;
+    std::string outputFilePath = "output.csv";
+    int numHilos = 4;
+    
+    int opt;
+    while ((opt = getopt(argc, argv, "i:o:t:")) != -1) {
+        switch (opt) {
+            case 'i':
+                inputFilePath = optarg;
+                break;
+            case 'o':
+                outputFilePath = optarg;
+                break;
+            case 't':
+                numHilos = std::stoi(optarg);
+                break;
+            default:
+                std::cerr << "Uso: " << argv[0] << " -i <inputFile> -o <outputFile> -t <numHilos>" << std::endl;
+                return 1;
+        }
+    }
+    
+    if (inputFilePath.empty()) {
+        std::cerr << "Error: El archivo de entrada es obligatorio." << std::endl;
+        return 1;
+    }
+    
+    generarCSV(inputFilePath, outputFilePath, numHilos);
+    
+    return 0;
+}
+
